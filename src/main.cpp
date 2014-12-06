ada07bf083c0ce70e30071eafbd51ff4f10629b1
Sasamus <labus93@gmail.com>
2014-12-06 13:09:58 +0100

    Created an Eclipse CTD project

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Albin Engström
+// Description : main of project dining-philosophers
+// Purpose	   : Start the program
+//============================================================================
+
+
+using namespace std;
+
+int main() {
+
+
+
+	return 0;
+}
