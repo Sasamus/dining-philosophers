4982362747408bf0d0d7d49fc3ea19c3854e38f9
Sasamus <labus93@gmail.com>
2014-12-07 16:58:59 +0100

    Created class Table

---
+++ src/Table.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Table.cpp
+// Author      : Albin Engström
+// Description : Implementation of class Table
+//============================================================================
+
+#include "Table.h"
+
+Table::Table() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Table::~Table() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Table.h
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Table.h
+// Author      : Albin Engström
+// Description : Declaration of class Table
+// Purpose	   : Handle the state of the chopsticks
+//============================================================================
+
+#ifndef TABLE_H_
+#define TABLE_H_
+
+/*
+ *
+ */
+class Table {
+public:
+	Table();
+	virtual ~Table();
+};
+
+#endif /* TABLE_H_ */
